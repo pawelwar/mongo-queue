681fb6fa7d964e0affcd119ceabf822af766e2ef
Pawel Warczynski <pawel.warczynski@allegro.pl>
2018-07-20 19:57:41 +0200

    Just a Spring Boot with MongoDB dependency

---
+++ src/main/java/wareq/mongoqueue/MongoqueueApplication.java
@@ -0,0 +1,12 @@
+package wareq.mongoqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoqueueApplication.class, args);
+	}
+}
